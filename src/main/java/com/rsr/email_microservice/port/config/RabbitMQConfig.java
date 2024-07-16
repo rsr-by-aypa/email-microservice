96352567b4847e28903ae21396a26cbb83b86d8a
Grischakrombacha <grischa.storch@gmail.com>
2024-07-16 20:23:17 +0200

    AYPA50 and AYPA51: Send Order Email

--- src/main/java/com/rsr/email_microservice/port/config/RabbitMQConfig.java
+++ src/main/java/com/rsr/email_microservice/port/config/RabbitMQConfig.java
@@ -2,6 +2,7 @@ package com.rsr.email_microservice.port.config;
 
 import org.springframework.amqp.core.*;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
@@ -43,7 +44,7 @@ public class RabbitMQConfig {
 
     @Bean
     public TopicExchange exchange() {
-        return new TopicExchange(exchange);
+        return new TopicExchange(exchange, true, false);
     }
 
     @Bean
@@ -75,4 +76,8 @@ public class RabbitMQConfig {
         return rabbitTemplate;
     }
 
+    @Bean
+    public RabbitAdmin rabbitAdmin(ConnectionFactory connectionFactory) {
+        return new RabbitAdmin(connectionFactory);
+    }
 }
