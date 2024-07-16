81007d9886a7e94d8f09a27ef25f34ad9cacbc11
Grischakrombacha <grischa.storch@gmail.com>
2024-07-17 00:07:37 +0200

    Adjusted Test

--- src/test/java/com/rsr/email_microservice/EmailMicroserviceApplicationTests.java
+++ src/test/java/com/rsr/email_microservice/EmailMicroserviceApplicationTests.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Import;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.containers.RabbitMQContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
@@ -19,8 +20,24 @@ class EmailMicroserviceApplicationTests {
 	@Container
 	public static RabbitMQContainer rabbitMQContainer = new RabbitMQContainer("rabbitmq:latest");
 
+	@Container
+	private static PostgreSQLContainer<?> postgresqlContainer = new PostgreSQLContainer<>("postgres:13.3")
+			.withDatabaseName("test")
+			.withUsername("test")
+			.withPassword("test");
+
 	@DynamicPropertySource
 	static void dynamicProperties(DynamicPropertyRegistry registry) {
+		String dbUrl = postgresqlContainer.getJdbcUrl();
+		String username = postgresqlContainer.getUsername();
+		String password = postgresqlContainer.getPassword();
+
+		registry.add("spring.datasource.url",
+				() -> dbUrl);
+		registry.add("spring.datasource.username",
+				() -> username);
+		registry.add("spring.datasource.password",
+				() -> password);
 
 		registry.add("spring.rabbitmq.host", rabbitMQContainer::getHost);
 		registry.add("spring.rabbitmq.port", rabbitMQContainer::getAmqpPort);
